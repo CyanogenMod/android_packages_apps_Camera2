63764789fdef4dfcdb3c0e1a9fa46b5d62312f07
Chih-Chung Chang <chihchung@google.com>
2012-03-08 20:10:53 +0800

    Merge Camera into Gallery package.
    Change-Id: If66d4b841eec3d3f5384a561b07c0eeade92597a

--- Android.mk
+++ Android.mk
@@ -8,17 +8,29 @@ LOCAL_STATIC_JAVA_LIBRARIES += com.android.gallery3d.common2
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_SRC_FILES += $(call all-java-files-under, src_pd)
+LOCAL_SRC_FILES += $(call all-java-files-under, ../Camera/src)
+
+LOCAL_RESOURCE_DIR += $(LOCAL_PATH)/res packages/apps/Camera/res
+LOCAL_AAPT_FLAGS := --auto-add-overlay --extra-packages com.android.camera
 
 LOCAL_PACKAGE_NAME := Gallery2
 
 LOCAL_OVERRIDES_PACKAGES := Gallery Gallery3D GalleryNew3D
 
-# We mark this out until Mtp and MediaMetadataRetriever is unhidden.
-LOCAL_SDK_VERSION := current
+#LOCAL_SDK_VERSION := current
+
+LOCAL_JNI_SHARED_LIBRARIES := libjni_mosaic
+
+LOCAL_REQUIRED_MODULES := libjni_mosaic
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 include $(BUILD_PACKAGE)
 
-# Use the following include to make our test apk.
+ifeq ($(strip $(LOCAL_PACKAGE_OVERRIDES)),)
+# Use the following include to make gallery test apk.
 include $(call all-makefiles-under,$(LOCAL_PATH))
+
+# Use the following include to make camera test apk.
+include $(call all-makefiles-under, ../Camera)
+endif
