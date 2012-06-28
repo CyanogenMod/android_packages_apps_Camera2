3a45725ad42324cd2db172fb082fe80772d94d3f
Owen Lin <owenlin@google.com>
2012-06-28 11:10:02 +0800

    Don't use Build.VERSION_CODES.
    Since they won't be available on old platform.
    
    bug: 6752318
    
    Change-Id: I55a9e83dbf05b93c83c68cb31f6edcbc29793283

--- src/com/android/gallery3d/data/LocalImage.java
+++ src/com/android/gallery3d/data/LocalImage.java
@@ -25,7 +25,6 @@ import android.graphics.BitmapFactory;
 import android.graphics.BitmapRegionDecoder;
 import android.media.ExifInterface;
 import android.net.Uri;
-import android.os.Build;
 import android.provider.MediaStore.Images;
 import android.provider.MediaStore.Images.ImageColumns;
 import android.provider.MediaStore.MediaColumns;
@@ -86,7 +85,7 @@ public class LocalImage extends LocalMediaItem {
         updateWidthAndHeightProjection();
     }
 
-    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    @TargetApi(ApiHelper.VERSION_CODES.JELLY_BEAN)
     private static void updateWidthAndHeightProjection() {
         if (ApiHelper.HAS_MEDIA_COLUMNS_WIDTH_AND_HEIGHT) {
             PROJECTION[INDEX_WIDTH] = MediaColumns.WIDTH;
