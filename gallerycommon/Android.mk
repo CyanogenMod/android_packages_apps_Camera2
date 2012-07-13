1ce42a976d0abfbc0f4a7d2f76fd01e718b6a54d
Ying Wang <wangying@google.com>
2012-07-13 10:19:43 -0700

    Change "current" sdk version to 16 as it becomes official
    Change-Id: I09c13b99ff1bc368e1fa335f2efc4cba5410d2f5

--- gallerycommon/Android.mk
+++ gallerycommon/Android.mk
@@ -22,6 +22,6 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := com.android.gallery3d.common2
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
-LOCAL_SDK_VERSION := current
+LOCAL_SDK_VERSION := 16
 
 include $(BUILD_STATIC_JAVA_LIBRARY)
