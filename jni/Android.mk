4c8abc39a0974a062a1c1dbcebc085f7ebcd1736
Andrew Hsieh <andrewhsieh@google.com>
2012-07-18 16:40:14 -0700

    Fixed x86 build
    NDK supports x86 from r6.  Without setting correct LOCAL_NDK_VERSION
    build may fail to add library path for "-llog" and "-lEGL"
    
    Change-Id: I863e20e434d274b5d8924eafcf1f5e1d0b05f90d

--- jni/Android.mk
+++ jni/Android.mk
@@ -9,10 +9,14 @@ LOCAL_SRC_FILES := jni_egl_fence.cpp
 ifeq ($(TARGET_ARCH), arm)
         LOCAL_NDK_VERSION := 5
         LOCAL_SDK_VERSION := 9
-        LOCAL_LDFLAGS :=  -llog -lEGL
-else
-        LOCAL_SHARED_LIBRARIES :=  liblog libEGL
 endif
+
+ifeq ($(TARGET_ARCH), x86)
+        LOCAL_NDK_VERSION := 6
+        LOCAL_SDK_VERSION := 9
+endif
+
+LOCAL_LDFLAGS :=  -llog -lEGL
 
 LOCAL_MODULE_TAGS := optional
 
