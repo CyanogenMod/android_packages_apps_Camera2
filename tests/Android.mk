d0af76e010fd959b1e97ef8e3a9e96dd50343db7
Earl Ou <shunhsingou@google.com>
2012-07-31 08:50:59 +0800

    Fix Gallery2Tests Build
    Remove dependancy of android-test-runner in Android.mk
    Also LOCAL_SDK_VERSION is needed in Android.mk for unbundle-branches
    to avoid hiden api
    
    Change-Id: I38cef8261aab264fe5a109ca5a6857eee3dff11b

--- tests/Android.mk
+++ tests/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 # We only want this apk build for tests.
 LOCAL_MODULE_TAGS := tests
 
-LOCAL_JAVA_LIBRARIES := android.test.runner
+LOCAL_SDK_VERSION := 16
 
 # Include all test java files.
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
