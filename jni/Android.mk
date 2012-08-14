4c47cca7e2beb1fd4109e18f78c4288392a2ca94
Ying Wang <wangying@google.com>
2012-08-14 10:33:44 -0700

    Retire LOCAL_NDK_VERSION.
    Bug: 6932421
    Change-Id: I9e5e8fba22ac235701c002e27012bb3954896140

--- jni/Android.mk
+++ jni/Android.mk
@@ -7,12 +7,10 @@ LOCAL_CFLAGS += -DEGL_EGLEXT_PROTOTYPES
 LOCAL_SRC_FILES := jni_egl_fence.cpp
 
 ifeq ($(TARGET_ARCH), arm)
-        LOCAL_NDK_VERSION := 5
         LOCAL_SDK_VERSION := 9
 endif
 
 ifeq ($(TARGET_ARCH), x86)
-        LOCAL_NDK_VERSION := 6
         LOCAL_SDK_VERSION := 9
 endif
 
