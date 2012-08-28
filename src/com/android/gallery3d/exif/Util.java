4f529e7cada294befb66a1fc9b72b1aa164597df
Earl Ou <shunhsingou@google.com>
2012-08-28 18:39:49 +0800

    ExifOutputStream
    Change-Id: I9f2de77e87a502ccdadba0b18658621028c538c9

--- src/com/android/gallery3d/exif/Util.java
+++ src/com/android/gallery3d/exif/Util.java
@@ -16,8 +16,19 @@
 
 package com.android.gallery3d.exif;
 
+import java.io.Closeable;
+
 class Util {
     public static boolean equals(Object a, Object b) {
         return (a == b) || (a == null ? false : a.equals(b));
+    }
+
+    public static void closeSilently(Closeable c) {
+        if (c == null) return;
+        try {
+            c.close();
+        } catch (Throwable t) {
+            // do nothing
+        }
     }
 }
