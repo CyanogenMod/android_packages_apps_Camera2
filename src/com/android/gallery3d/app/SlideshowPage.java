b02977e128c7b85ad964c3ed90a6c5ed26ac682d
Wu-cheng Li <wuchengli@google.com>
2012-08-30 16:44:32 +0800

    Use FLAG_SHOW_WHEN_LOCKED for secure album.
    The activity needs this flag to be on top of the lock screen
    when the camera is started from the secure lock screen.
    
    bug:5955016
    
    Change-Id: If554a3f7fef4c03a981c83afe22b8c4980b954d1

--- src/com/android/gallery3d/app/SlideshowPage.java
+++ src/com/android/gallery3d/app/SlideshowPage.java
@@ -112,7 +112,8 @@ public class SlideshowPage extends ActivityState {
     @Override
     public void onCreate(Bundle data, Bundle restoreState) {
         super.onCreate(data, restoreState);
-        mFlags |= (FLAG_HIDE_ACTION_BAR | FLAG_HIDE_STATUS_BAR);
+        mFlags |= (FLAG_HIDE_ACTION_BAR | FLAG_HIDE_STATUS_BAR
+                | FLAG_ALLOW_LOCK_WHILE_SCREEN_ON | FLAG_SHOW_WHEN_LOCKED);
         if (data.getBoolean(KEY_DREAM)) {
             // Dream screensaver only keeps screen on for plugged devices.
             mFlags |= FLAG_SCREEN_ON_WHEN_PLUGGED;
