93dfe7a1df07a64701369b480578410efb740a27
nicolasroard <nicolasroard@google.com>
2012-10-08 21:04:48 -0700

    Add procedural borders
    bug:7233980
    Change-Id: I81008efa3d256061ec27d1ca2333debfb0ab6dd5

--- src/com/android/gallery3d/filtershow/imageshow/ImageSmallFilter.java
+++ src/com/android/gallery3d/filtershow/imageshow/ImageSmallFilter.java
@@ -1,18 +1,14 @@
 
 package com.android.gallery3d.filtershow.imageshow;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.AttributeSet;
+import android.view.View;
+
 import com.android.gallery3d.filtershow.FilterShowActivity;
 import com.android.gallery3d.filtershow.filters.ImageFilter;
 import com.android.gallery3d.filtershow.presets.ImagePreset;
-
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Paint;
-import android.util.AttributeSet;
-import android.util.Log;
-import android.view.View;
-import android.view.View.MeasureSpec;
 
 public class ImageSmallFilter extends ImageShow implements View.OnClickListener {
 
@@ -20,6 +16,7 @@ public class ImageSmallFilter extends ImageShow implements View.OnClickListener 
     private FilterShowActivity mController = null;
     private ImageFilter mImageFilter = null;
     private boolean mShowTitle = true;
+    private boolean mSetBorder = false;
 
     public ImageSmallFilter(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -37,26 +34,33 @@ public class ImageSmallFilter extends ImageShow implements View.OnClickListener 
         mImagePreset.add(mImageFilter);
     }
 
+    public void setBorder(boolean value) {
+        mSetBorder = value;
+    }
+
     public void setController(FilterShowActivity activity) {
         mController = activity;
     }
 
+    @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         int parentWidth = MeasureSpec.getSize(widthMeasureSpec);
         int parentHeight = MeasureSpec.getSize(heightMeasureSpec);
         setMeasuredDimension(parentHeight, parentHeight);
     }
 
+    @Override
     public void onClick(View v) {
         if (mController != null) {
             if (mImageFilter != null) {
-                mController.useImageFilter(mImageFilter);
+                mController.useImageFilter(mImageFilter, mSetBorder);
             } else if (mImagePreset != null) {
                 mController.useImagePreset(mImagePreset);
             }
         }
     }
 
+    @Override
     public Bitmap getOriginalFrontBitmap() {
         if (mImageLoader == null) {
             return null;
@@ -69,14 +73,17 @@ public class ImageSmallFilter extends ImageShow implements View.OnClickListener 
         invalidate();
     }
 
+    @Override
     public boolean showTitle() {
         return mShowTitle;
     }
 
+    @Override
     public boolean showControls() {
         return false;
     }
 
+    @Override
     public boolean showHires() {
         return false;
     }
