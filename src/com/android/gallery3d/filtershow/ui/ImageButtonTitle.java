5ec40a6ba587fa8bd50958d263bb6fc6280de4c1
nicolasroard <nicolasroard@google.com>
2012-10-13 18:38:51 -0700

    Fix dimensions to use dips instead of pixels
    bug:7343798
    Change-Id: I3d576ae334e580ad9da5b56d4ae84f749004ec21

--- src/com/android/gallery3d/filtershow/ui/ImageButtonTitle.java
+++ src/com/android/gallery3d/filtershow/ui/ImageButtonTitle.java
@@ -1,15 +1,14 @@
 
 package com.android.gallery3d.filtershow.ui;
 
-import com.android.gallery3d.R;
-
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.widget.ImageButton;
+
+import com.android.gallery3d.R;
 
 public class ImageButtonTitle extends ImageButton {
     private static final String LOGTAG = "ImageButtonTitle";
@@ -17,6 +16,14 @@ public class ImageButtonTitle extends ImageButton {
     private static int mTextSize = 24;
     private static int mTextPadding = 20;
     private static Paint gPaint = new Paint();
+
+    public static void setTextSize(int value) {
+        mTextSize = value;
+    }
+
+    public static void setTextPadding(int value) {
+        mTextPadding = value;
+    }
 
     public ImageButtonTitle(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -26,6 +33,7 @@ public class ImageButtonTitle extends ImageButton {
         mText = a.getString(R.styleable.ImageButtonTitle_android_text);
     }
 
+    @Override
     public void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         if (mText != null) {
