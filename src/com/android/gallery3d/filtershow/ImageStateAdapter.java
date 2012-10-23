983f612476c394dffcd2b73869ed3c92f531e5e1
nicolasroard <nicolasroard@google.com>
2012-10-23 11:26:38 -0700

    Add copyright notices
    bug:7387634
    Change-Id: Iaac0b55c9857f609312b78a613d074d323174ea1

--- src/com/android/gallery3d/filtershow/ImageStateAdapter.java
+++ src/com/android/gallery3d/filtershow/ImageStateAdapter.java
@@ -1,17 +1,30 @@
+/*
+ * Copyright (C) 2012 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.android.gallery3d.filtershow;
 
-import com.android.gallery3d.R;
-import com.android.gallery3d.filtershow.filters.ImageFilter;
-import com.android.gallery3d.filtershow.presets.ImagePreset;
-
 import android.content.Context;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+
+import com.android.gallery3d.R;
+import com.android.gallery3d.filtershow.filters.ImageFilter;
 
 public class ImageStateAdapter extends ArrayAdapter<ImageFilter> {
     private static final String LOGTAG = "ImageStateAdapter";
@@ -20,6 +33,7 @@ public class ImageStateAdapter extends ArrayAdapter<ImageFilter> {
         super(context, textViewResourceId);
     }
 
+    @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         View view = convertView;
         if (view == null) {
