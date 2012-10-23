a9f280f938b5fd5891c5cfe0999f8f1d4945d7a1
nicolasroard <nicolasroard@google.com>
2012-10-23 11:26:38 -0700

    Add copyright notices
    bug:7387634
    Change-Id: Iaac0b55c9857f609312b78a613d074d323174ea1

--- src/com/android/gallery3d/filtershow/CenteredLinearLayout.java
+++ src/com/android/gallery3d/filtershow/CenteredLinearLayout.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2012 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.android.gallery3d.filtershow;
 
 import android.content.Context;
@@ -5,7 +21,6 @@ import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.util.TypedValue;
-import android.view.View.MeasureSpec;
 import android.widget.LinearLayout;
 
 import com.android.gallery3d.R;
