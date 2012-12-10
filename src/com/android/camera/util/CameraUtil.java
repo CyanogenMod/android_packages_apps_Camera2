905fc8154d5509c7068c2af64ce425771dc59388
codeworkx <codeworkx@cyanogenmod.org>
2012-12-10 22:50:41 +0100

    camera: add support for samsung and htc camcorder modes
    Conflicts:
    
    	res/values/config.xml
    	src/com/android/camera/CameraSettings.java
    	src/com/android/camera/VideoModule.java
    	src/com/android/camera/util/CameraUtil.java
    
    Change-Id: I56739157380f596c9f3bbbe7aecd8f532d619c72

--- src/com/android/camera/util/CameraUtil.java
+++ src/com/android/camera/util/CameraUtil.java
@@ -156,6 +156,12 @@ public class CameraUtil {
     private static float sPixelDensity = 1;
     private static ImageFileNamer sImageFileNamer;
 
+    // Samsung camcorder mode
+    private static boolean sSamsungCamMode;
+
+    // HTC camcorder mode
+    private static boolean sHTCCamMode;
+
     private CameraUtil() {
     }
 
@@ -167,10 +173,22 @@ public class CameraUtil {
         sPixelDensity = metrics.density;
         sImageFileNamer = new ImageFileNamer(
                 context.getString(R.string.image_file_name_format));
+
+        // These come from the config, but are needed before parameters are set.
+        sSamsungCamMode = context.getResources().getBoolean(R.bool.needsSamsungCamMode);
+        sHTCCamMode = context.getResources().getBoolean(R.bool.needsHTCCamMode);
     }
 
     public static int dpToPixel(int dp) {
         return Math.round(sPixelDensity * dp);
+    }
+
+    public static boolean useHTCCamMode() {
+        return sHTCCamMode;
+    }
+
+    public static boolean useSamsungCamMode() {
+        return sSamsungCamMode;
     }
 
     // Rotates the bitmap by the specified degree.
