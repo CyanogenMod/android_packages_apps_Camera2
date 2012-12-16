a91be8aeac4bc1f4474d920eded8e4760402364f
Steve Kondik <shade@chemlab.org>
2012-12-15 20:31:59 -0800

    camera: Add ZSL support for Samsung/Qualcomm cameras
     * Preview management for Samsung/Qualcomm cameras
    
    Change-Id: Ib36bd21c9a76b45bced3eee2f25acc35b5d82b30

--- src/com/android/camera/util/CameraUtil.java
+++ src/com/android/camera/util/CameraUtil.java
@@ -165,6 +165,12 @@ public class CameraUtil {
     // For setting video size before recording starts
     private static boolean sEarlyVideoSize;
 
+    // Workaround for QC cameras with broken face detection on front camera
+    private static boolean sNoFaceDetectOnFrontCamera;
+
+    // Samsung ZSL mode
+    private static boolean sEnableZSL;
+
     private CameraUtil() {
     }
 
@@ -181,6 +187,8 @@ public class CameraUtil {
         sSamsungCamMode = context.getResources().getBoolean(R.bool.needsSamsungCamMode);
         sHTCCamMode = context.getResources().getBoolean(R.bool.needsHTCCamMode);
         sEarlyVideoSize = context.getResources().getBoolean(R.bool.needsEarlyVideoSize);
+        sEnableZSL = context.getResources().getBoolean(R.bool.enableZSL);
+        sNoFaceDetectOnFrontCamera = context.getResources().getBoolean(R.bool.noFaceDetectOnFrontCamera);
     }
 
     public static int dpToPixel(int dp) {
@@ -197,6 +205,14 @@ public class CameraUtil {
 
     public static boolean needsEarlyVideoSize() {
         return sEarlyVideoSize;
+    }
+
+    public static boolean enableZSL() {
+        return sEnableZSL;
+    }
+
+    public static boolean noFaceDetectOnFrontCamera() {
+        return sNoFaceDetectOnFrontCamera;
     }
 
     // Rotates the bitmap by the specified degree.
