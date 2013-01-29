2c87db720ca1d6b76260ec86a5bbf29a19eb2c2f
Michael Kolb <kolby@google.com>
2013-01-29 14:03:06 -0800

    Copy camera resources into Gallery2
    Change-Id: I3be2758b2ddca31dbc2f21457a62664310c39684

--- Android.mk
+++ Android.mk
@@ -12,8 +12,7 @@ LOCAL_STATIC_JAVA_LIBRARIES += mp4parser
 LOCAL_SRC_FILES := $(call all-java-files-under, src) $(call all-renderscript-files-under, src)
 LOCAL_SRC_FILES += $(call all-java-files-under, src_pd)
 
-LOCAL_RESOURCE_DIR += $(LOCAL_PATH)/res 
-LOCAL_RESOURCE_DIR += packages/apps/Camera/res
+LOCAL_RESOURCE_DIR += $(LOCAL_PATH)/res
 
 LOCAL_AAPT_FLAGS := --auto-add-overlay \
     --extra-packages com.android.camera
