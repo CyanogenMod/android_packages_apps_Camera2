1ddbdc88f9b6e8ab26ba344c5bebb5b6fa220941
Tim Murray <timmurray@google.com>
2013-02-04 13:09:27 -0800

    Switch Gallery over to RS compatibility library.
    Change-Id: If27809236534f3b196949bf455faed45c99f3da5

--- src/com/android/gallery3d/filtershow/cache/FilteringPipeline.java
+++ src/com/android/gallery3d/filtershow/cache/FilteringPipeline.java
@@ -19,9 +19,7 @@ package com.android.gallery3d.filtershow.cache;
 import android.graphics.Bitmap;
 import android.os.*;
 import android.os.Process;
-import android.renderscript.Allocation;
-import android.renderscript.Allocation.MipmapControl;
-import android.renderscript.RenderScript;
+import android.support.v8.renderscript.*;
 import android.util.Log;
 
 import com.android.gallery3d.filtershow.filters.ImageFilterRS;
@@ -164,13 +162,13 @@ public class FilteringPipeline implements Handler.Callback {
             mFiltersOnlyOriginalAllocation.destroy();
         }
         mFiltersOnlyOriginalAllocation = Allocation.createFromBitmap(RS, mOriginalBitmap,
-                MipmapControl.MIPMAP_NONE, Allocation.USAGE_SCRIPT);
+                Allocation.MipmapControl.MIPMAP_NONE, Allocation.USAGE_SCRIPT);
         if (mOriginalAllocation != null) {
             mOriginalAllocation.destroy();
         }
         mResizedOriginalBitmap = preset.applyGeometry(mOriginalBitmap);
         mOriginalAllocation = Allocation.createFromBitmap(RS, mResizedOriginalBitmap,
-                MipmapControl.MIPMAP_NONE, Allocation.USAGE_SCRIPT);
+                Allocation.MipmapControl.MIPMAP_NONE, Allocation.USAGE_SCRIPT);
         mPreviousGeometry = new GeometryMetadata(geometry);
         return true;
     }
