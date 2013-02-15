824f9fa8bfb8c3c41de865dcff3898bcb73e74e9
Victor Doba <dobavictor@google.com>
2013-02-15 13:24:41 +0100

    Show the Utility Panel by default.

--- src/com/android/gallery3d/filtershow/imageshow/ImageShow.java
+++ src/com/android/gallery3d/filtershow/imageshow/ImageShow.java
@@ -673,7 +673,7 @@ public class ImageShow extends View implements OnGestureListener,
     }
 
     public boolean useUtilityPanel() {
-        return false;
+        return true;
     }
 
     public void openUtilityPanel(final LinearLayout accessoryViewList) {
