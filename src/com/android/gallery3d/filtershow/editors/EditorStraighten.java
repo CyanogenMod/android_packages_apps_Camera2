d61a2f9fcaad0309132b6b9b666c3dc6df62fed7
John Hoford <hoford@google.com>
2013-02-27 08:41:19 -0800

    refactor the geometry code
    Change-Id: I5b0381ec5e127aabf37924af64fbeb490755bc9a

---
+++ src/com/android/gallery3d/filtershow/editors/EditorStraighten.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.gallery3d.filtershow.editors;
+
+import android.content.Context;
+import android.widget.FrameLayout;
+
+import com.android.gallery3d.R;
+import com.android.gallery3d.filtershow.imageshow.ImageStraighten;
+import com.android.gallery3d.filtershow.imageshow.MasterImage;
+
+public class EditorStraighten extends Editor implements EditorInfo {
+    public static final int ID = R.id.editorStraighten;
+    ImageStraighten mImageStraighten;
+
+    public EditorStraighten() {
+        super(ID);
+    }
+
+    @Override
+    public void createEditor(Context context, FrameLayout frameLayout) {
+        super.createEditor(context, frameLayout);
+        mView = mImageShow = mImageStraighten = new ImageStraighten(context);
+        mImageStraighten.setImageLoader(MasterImage.getImage().getImageLoader());
+        mImageStraighten.setEditor(this);
+        mImageStraighten.syncLocalToMasterGeometry();
+    }
+
+    @Override
+    public int getTextId() {
+        return R.string.straighten;
+    }
+
+    @Override
+    public int getOverlayId() {
+        return R.drawable.filtershow_button_geometry_straighten;
+    }
+
+    @Override
+    public boolean getOverlayOnly() {
+        return true;
+    }
+}
