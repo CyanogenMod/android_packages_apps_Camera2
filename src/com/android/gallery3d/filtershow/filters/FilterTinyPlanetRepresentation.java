55afc2dcadf2f88774fbbe2eb7b11a5639b9fa29
John Hoford <hoford@google.com>
2013-04-18 18:43:29 -0700

    tiny planet fix
    bug:8323524
    Change-Id: I39283face7079574dbe25e797323a84141930f9a

--- src/com/android/gallery3d/filtershow/filters/FilterTinyPlanetRepresentation.java
+++ src/com/android/gallery3d/filtershow/filters/FilterTinyPlanetRepresentation.java
@@ -31,6 +31,7 @@ public class FilterTinyPlanetRepresentation extends FilterBasicRepresentation {
         setTextId(R.string.tinyplanet);
         setButtonId(R.id.tinyplanetButton);
         setEditorId(EditorTinyPlanet.ID);
+        setMinimum(1);
     }
 
     @Override
@@ -40,6 +41,14 @@ public class FilterTinyPlanetRepresentation extends FilterBasicRepresentation {
         representation.mAngle = mAngle;
         representation.setZoom(getZoom());
         return representation;
+    }
+
+    @Override
+    public void useParametersFrom(FilterRepresentation a) {
+        FilterTinyPlanetRepresentation representation = (FilterTinyPlanetRepresentation) a;
+        super.useParametersFrom(a);
+        mAngle = representation.mAngle;
+        setZoom(representation.getZoom());
     }
 
     public void setAngle(float angle) {
