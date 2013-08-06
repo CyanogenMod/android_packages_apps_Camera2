43ec6facaec9fdfa7b29ea3cce114e94af95c34c
Sascha Haeberling <haeberling@google.com>
2013-08-06 16:44:42 -0700

    Add .mk files for Camera2.
    Change-Id: I0678c1b12f40f6dfd76d7589f61d18e99997394f

---
+++ Android.mk
@@ -0,0 +1,27 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_STATIC_JAVA_LIBRARIES := android-support-v13
+LOCAL_STATIC_JAVA_LIBRARIES += com.android.camera.common
+LOCAL_STATIC_JAVA_LIBRARIES += xmp_toolkit
+
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+LOCAL_SRC_FILES += $(call all-java-files-under, src_pd)
+
+LOCAL_RESOURCE_DIR += $(LOCAL_PATH)/res
+
+LOCAL_AAPT_FLAGS := --auto-add-overlay
+
+LOCAL_PACKAGE_NAME := Camera2
+
+LOCAL_SDK_VERSION := current
+
+LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+
+include $(BUILD_PACKAGE)
+
+include $(call all-makefiles-under, $(LOCAL_PATH))
