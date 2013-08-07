eee0d4e9b1c65f86ecbb303b759868ccbf9de597
Sascha Haeberling <haeberling@google.com>
2013-08-07 16:40:00 -0700

    Changes required for building GoogleCamera.
    Change-Id: I2ea7f4714e3003ff05eadb6cb8b93af721b39ac6

--- src/com/android/camera/app/CameraApp.java
+++ src/com/android/camera/app/CameraApp.java
@@ -1,11 +1,43 @@
+/*
+ * Copyright (C) 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.android.camera.app;
 
+import com.android.camera.Util;
+import com.android.camera.util.PhotoSphereHelper;
+
 import android.app.Application;
+import android.content.Context;
 
 public class CameraApp extends Application {
-	@Override
-	public void onCreate() {
-		super.onCreate();
-		com.android.camera.Util.initialize(this);
-	}
+
+    private StitchingProgressManager mStitchingProgressManager;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Util.initialize(this);
+        mStitchingProgressManager = PhotoSphereHelper.createStitchingManagerInstance(this);
+    }
+
+    public Context getAndroidContext() {
+        return this;
+    }
+
+    public StitchingProgressManager getStitchingProgressManager() {
+        return mStitchingProgressManager;
+    }
 }
