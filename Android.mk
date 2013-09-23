283beaf677d0c3235fbb22bfd5cd8338459bed1b
Ruben Brunk <rubenbrunk@google.com>
2013-09-23 10:59:38 -0700

    Fix build: Don't build GCam for non-arm targets
    Bug: 10430748
    
    Change-Id: I26e1208b38f40808ff59e3976f5c4a76efa15b4c

--- Android.mk
+++ Android.mk
@@ -10,6 +10,7 @@ LOCAL_STATIC_JAVA_LIBRARIES += xmp_toolkit
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_SRC_FILES += $(call all-java-files-under, src_pd)
+LOCAL_SRC_FILES += $(call all-java-files-under, src_pd_gcam)
 
 LOCAL_RESOURCE_DIR += $(LOCAL_PATH)/res
 
