9764cbf40f9bd927a00f3a9b9e32485547e61109
Alok Kediya <kediya@codeaurora.org>
2013-09-28 18:40:55 +0530

    camera: change for zsl and hdr incompatible
    Camera will crash when capture in zsl and hdr mode sametime, change
    to fix this issue through enabling one mode will disable the other one.
    
    (cherry picked from commit 4e4301d6cd56e710d1d628be23bcb1477759fd9b)
    
    Change-Id: I1a9ceaa9698c01246e98284e25ff34c50bf5fc74
    (cherry picked from commit f208f2709eb452fc956b5ee8349d41b09f8d2837)
    (cherry picked from commit 4f7013911538ba244a1333d301ce5388a6f60769)
    (cherry picked from commit 0917909d7fc6fc3a2084a5cdafea68d1c2d0bc82)

--- src/com/android/camera/PhotoMenu.java
+++ src/com/android/camera/PhotoMenu.java
@@ -199,9 +199,14 @@ public class PhotoMenu extends PieController
         // set to non-auto.
         if (notSame(pref, CameraSettings.KEY_CAMERA_HDR, mSettingOff)) {
             setPreference(CameraSettings.KEY_SCENE_MODE, Parameters.SCENE_MODE_AUTO);
+            setPreference(CameraSettings.KEY_ZSL,mSettingOff);
         } else if (notSame(pref, CameraSettings.KEY_SCENE_MODE, Parameters.SCENE_MODE_AUTO)) {
+            setPreference(CameraSettings.KEY_CAMERA_HDR, mSettingOff);
+        }
+        if (notSame(pref,CameraSettings.KEY_ZSL,mSettingOff)){
             setPreference(CameraSettings.KEY_CAMERA_HDR, mSettingOff);
         }
         super.onSettingChanged(pref);
     }
+
 }
