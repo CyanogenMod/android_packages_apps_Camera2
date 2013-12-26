b6fc0b79729e3bf09d2560d00fd4aa5d64f5402f
Ryuinferno <ryuinferno.xda@gmail.com>
2013-12-26 19:11:23 +0100

    Camera2: More thorough compatibility fixes
    Certain chipsets (such as omap) have contrast, sharpness and saturation
    set, but no max integer given for those
    
    Change-Id: I58efaef4d0a511aefc803a440e0c3aa3dbdc9483

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -2110,7 +2110,8 @@ public class PhotoModule
             mParameters.setColorEffect(colorEffect);
         }
         //Set Saturation
-        if (CameraUtil.isSupported(mParameters, "saturation")) {
+        if (CameraUtil.isSupported(mParameters, "saturation") &&
+                    CameraUtil.isSupported(mParameters, "saturation-max")) {
             String saturationStr = mPreferences.getString(
                     CameraSettings.KEY_SATURATION,
                     mActivity.getString(R.string.pref_camera_saturation_default));
@@ -2121,7 +2122,8 @@ public class PhotoModule
             }
         }
         // Set contrast parameter.
-        if (CameraUtil.isSupported(mParameters, "contrast")) {
+        if (CameraUtil.isSupported(mParameters, "contrast") &&
+                    CameraUtil.isSupported(mParameters, "contrast-max")) {
             String contrastStr = mPreferences.getString(
                     CameraSettings.KEY_CONTRAST,
                     mActivity.getString(R.string.pref_camera_contrast_default));
@@ -2132,7 +2134,8 @@ public class PhotoModule
             }
         }
         // Set sharpness parameter
-        if (CameraUtil.isSupported(mParameters, "sharpness")) {
+        if (CameraUtil.isSupported(mParameters, "sharpness") &&
+                    CameraUtil.isSupported(mParameters, "sharpness-max")) {
             String sharpnessStr = mPreferences.getString(
                     CameraSettings.KEY_SHARPNESS,
                     mActivity.getString(R.string.pref_camera_sharpness_default));
