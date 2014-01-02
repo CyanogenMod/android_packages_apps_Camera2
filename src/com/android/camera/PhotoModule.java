CyanogenMod/android_packages_apps_Camera2

commit f5b1181157b6d7f65376c0068768f8f3f94b5d69

Camera2: Adds support for longshot/burst mode
- The longshot/burst pipeline mode uses an
  optimized path for triggering 'takePicture()'
  on each shutter callback. This should improve
  the shot-2-shot time. In addition to this
  there is also support for jpeg callbacks
  that only contain a jpeg file path. The
  callback in this case will only move the
  file stored by the lower layers in the
  correct directory.
- The longshot burst pipeline can be enabled
  via this property:
  "persist.camera.longshot.enable"<-"0/1"
- This change will allow longshot mode to
  be triggered in non-zsl mode as well.
(Cherry picked from: I693366a7d06d3b386a8d96f86ee9a0574749c50b)
(Cherry picked from: Id630b2033f18d1c04a636597e910e695a8692ac8)

Change-Id: Idda8d58fc6d889128c1812c9c5ddadca3993c246

src/com/android/camera/PhotoModule.java
@@ -178,6 +178,7 @@ public class PhotoModule
     private boolean mAwbLockSupported;
     private boolean mContinuousFocusSupported;
     private boolean mTouchAfAecFlag;
+    private boolean mLongshotSave = false;
 
     // The degrees of the device rotated clockwise from its natural orientation.
     private int mOrientation = OrientationEventListener.ORIENTATION_UNKNOWN;
@@ -187,6 +188,9 @@ public class PhotoModule
 
     private ContentProviderClient mMediaProviderClient;
     private boolean mFaceDetectionStarted = false;
+
+    private static final String PERSIST_LONG_ENABLE = "persist.camera.longshot.enable";
+    private static final String PERSIST_LONG_SAVE = "persist.camera.longshot.save";
 
     private static final int MINIMUM_BRIGHTNESS = 0;
     private static final int MAXIMUM_BRIGHTNESS = 6;
@@ -756,6 +760,34 @@ public class PhotoModule
         }
     }
 
+    private final class LongshotShutterCallback
+            implements CameraShutterCallback {
+
+        @Override
+        public void onShutter(CameraProxy camera) {
+            mShutterCallbackTime = System.currentTimeMillis();
+            mShutterLag = mShutterCallbackTime - mCaptureStartTime;
+            Log.e(TAG, "[KPI Perf] PROFILE_SHUTTER_LAG mShutterLag = " + mShutterLag + "ms");
+            synchronized(mCameraDevice) {
+
+                if (mCameraState != LONGSHOT) {
+                    return;
+                }
+
+                if (mLongshotSave) {
+                    mCameraDevice.takePicture(mHandler,
+                            new LongshotShutterCallback(),
+                            mRawPictureCallback, mPostViewPictureCallback,
+                            new LongshotPictureCallback(null));
+                } else {
+                    mCameraDevice.takePicture(mHandler,new LongshotShutterCallback(),
+                            mRawPictureCallback, mPostViewPictureCallback,
+                            new JpegPictureCallback(null));
+                }
+            }
+        }
+    }
+
     private final class ShutterCallback
             implements CameraShutterCallback {
 
@@ -821,6 +853,64 @@ public class PhotoModule
         }
     }
 
+    private final class LongshotPictureCallback implements CameraPictureCallback {
+        Location mLocation;
+
+        public LongshotPictureCallback(Location loc) {
+            mLocation = loc;
+        }
+
+        @Override
+        public void onPictureTaken(final byte [] jpegData, CameraProxy camera) {
+            if (mPaused) {
+                return;
+            }
+
+            mFocusManager.updateFocusUI(); // Ensure focus indicator is hidden.
+
+            String jpegFilePath = new String(jpegData);
+            mNamedImages.nameNewImage(mCaptureStartTime);
+            NamedEntity name = mNamedImages.getNextNameEntity();
+            String title = (name == null) ? null : name.title;
+            long date = (name == null) ? -1 : name.date;
+
+            if (title == null) {
+                Log.e(TAG, "Unbalanced name/data pair");
+                return;
+            }
+
+
+            if  (date == -1 ) {
+                Log.e(TAG, "Invalid filename date");
+                return;
+            }
+
+            String dstPath = Storage.DIRECTORY;
+            File sdCard = android.os.Environment.getExternalStorageDirectory();
+            File dstFile = new File(dstPath);
+            if (dstFile == null) {
+                Log.e(TAG, "Destination file path invalid");
+                return;
+            }
+
+            File srcFile = new File(jpegFilePath);
+            if (srcFile == null) {
+                Log.e(TAG, "Source file path invalid");
+                return;
+            }
+
+            if ( srcFile.renameTo(dstFile) ) {
+                Size s = mParameters.getPictureSize();
+                String pictureFormat = mParameters.get(KEY_PICTURE_FORMAT);
+                mActivity.getMediaSaveService().addImage(
+                       null, title, date, mLocation, s.width, s.height,
+                       0, null, mOnMediaSavedListener, mContentResolver, pictureFormat);
+            } else {
+                Log.e(TAG, "Failed to move jpeg file");
+            }
+        }
+    }
+
     private final class JpegPictureCallback
             implements CameraPictureCallback {
         Location mLocation;
@@ -872,11 +962,13 @@ public class PhotoModule
             mFocusManager.updateFocusUI(); // Ensure focus indicator is hidden.
 
             boolean needRestartPreview = !mIsImageCaptureIntent
+                      && (mCameraState != LONGSHOT)
                       && (mSnapshotMode != CameraInfo.CAMERA_SUPPORT_MODE_ZSL)
                       && (mReceivedSnapNum == mBurstSnapNum);
             if (needRestartPreview) {
                 setupPreview();
-            }else if (mReceivedSnapNum == mBurstSnapNum){
+            }else if ((mReceivedSnapNum == mBurstSnapNum)
+                        && (mCameraState != LONGSHOT)){
                 mFocusManager.resetTouchFocus();
                 setCameraState(IDLE);
             }
@@ -1086,6 +1178,7 @@ public class PhotoModule
         switch (state) {
             case PhotoController.PREVIEW_STOPPED:
             case PhotoController.SNAPSHOT_IN_PROGRESS:
+            case PhotoController.LONGSHOT:
             case PhotoController.SWITCHING_CAMERA:
                 mUI.enableGestures(false);
                 break;
@@ -1162,15 +1255,30 @@ public class PhotoModule
         // We don't want user to press the button again while taking a
         // multi-second HDR photo.
         mUI.enableShutter(false);
-        mCameraDevice.takePicture(mHandler,
-                new ShutterCallback(!animateBefore),
-                mRawPictureCallback, mPostViewPictureCallback,
-                new JpegPictureCallback(loc));
+
+        if (mCameraState == LONGSHOT) {
+            if(mLongshotSave) {
+                mCameraDevice.takePicture(mHandler,
+                        new LongshotShutterCallback(),
+                        mRawPictureCallback, mPostViewPictureCallback,
+                        new LongshotPictureCallback(loc));
+            } else {
+                mCameraDevice.takePicture(mHandler,
+                        new LongshotShutterCallback(),
+                        mRawPictureCallback, mPostViewPictureCallback,
+                        new JpegPictureCallback(loc));
+            }
+        } else {
+            mCameraDevice.takePicture(mHandler,
+                    new ShutterCallback(!animateBefore),
+                    mRawPictureCallback, mPostViewPictureCallback,
+                    new JpegPictureCallback(loc));
+            setCameraState(SNAPSHOT_IN_PROGRESS);
+        }
 
         mNamedImages.nameNewImage(mCaptureStartTime);
 
         mFaceDetectionStarted = false;
-        setCameraState(SNAPSHOT_IN_PROGRESS);
         UsageStatistics.onEvent(UsageStatistics.COMPONENT_CAMERA,
                 UsageStatistics.ACTION_CAPTURE_DONE, "Photo");
         return true;
@@ -1375,6 +1483,18 @@ public class PhotoModule
                 || (mCameraState == SNAPSHOT_IN_PROGRESS)
                 || (mCameraState == PREVIEW_STOPPED)) return;
 
+        synchronized(mCameraDevice) {
+           if (mCameraState == LONGSHOT) {
+               mCameraDevice.setLongshot(false);
+               if (!mFocusManager.isZslEnabled()) {
+                   setupPreview();
+               } else {
+                   setCameraState(IDLE);
+                   mFocusManager.resetTouchFocus();
+               }
+           }
+        }
+
         // Do not do focus if there is not enough storage.
         if (pressed && !canTakePicture()) return;
 
@@ -1444,6 +1564,21 @@ public class PhotoModule
         } else {
             mSnapshotOnIdle = false;
             mFocusManager.doSnap();
+        }
+    }
+
+    @Override
+    public void onShutterButtonLongClick() {
+        if ((null != mCameraDevice) && (mCameraState == IDLE)) {
+            boolean enable = false;
+            enable = SystemProperties.getBoolean(PERSIST_LONG_ENABLE, false);
+            if ( enable ) {
+                enable = SystemProperties.getBoolean(PERSIST_LONG_SAVE, false);
+                mLongshotSave = enable;
+                mCameraDevice.setLongshot(true);
+                setCameraState(PhotoController.LONGSHOT);
+                mFocusManager.doSnap();
+            }
         }
     }
 

src/com/android/camera/ShutterButton.java
@@ -29,7 +29,18 @@ import android.widget.ImageView;
  */
 public class ShutterButton extends ImageView {
 
+    private class LongClickListener implements View.OnLongClickListener {
+         public boolean onLongClick(View v) {
+             if ( null != mListener ) {
+                 mListener.onShutterButtonLongClick();
+                 return true;
+             }
+             return false;
+         }
+    }
+
     private boolean mTouchEnabled = true;
+    private LongClickListener mLongClick = new LongClickListener();
 
     /**
      * A callback to be invoked when a ShutterButton's pressed state changes.
@@ -42,6 +53,7 @@ public class ShutterButton extends ImageView {
          */
         void onShutterButtonFocus(boolean pressed);
         void onShutterButtonClick();
+        void onShutterButtonLongClick();
     }
 
     private OnShutterButtonListener mListener;
@@ -53,6 +65,7 @@ public class ShutterButton extends ImageView {
 
     public void setOnShutterButtonListener(OnShutterButtonListener listener) {
         mListener = listener;
+        setOnLongClickListener(mLongClick);
     }
 
     @Override
@@ -66,6 +79,7 @@ public class ShutterButton extends ImageView {
 
     public void enableTouch(boolean enable) {
         mTouchEnabled = enable;
+        setLongClickable(enable);
     }
 
     /**

src/com/android/camera/WideAnglePanoramaUI.java
@@ -316,6 +316,9 @@ public class WideAnglePanoramaUI implements
     }
 
     @Override
+    public void onShutterButtonLongClick() {}
+
+    @Override
     public void onLayoutChange(
             View v, int l, int t, int r, int b,
             int oldl, int oldt, int oldr, int oldb) {

src/com/android/camera/AndroidCameraManagerImpl.java
@@ -88,6 +88,8 @@ class AndroidCameraManagerImpl implements CameraManager {
     // Histogram
     private static final int SET_HISTOGRAM_MODE =    601;
     private static final int SEND_HISTOGRAM_DATA =   602;
+    //LONGSHOT
+    private static final int SET_LONGSHOT = 701;
 
     private CameraHandler mCameraHandler;
     private android.hardware.Camera mCamera;
@@ -329,6 +331,11 @@ class AndroidCameraManagerImpl implements CameraManager {
                     case SEND_HISTOGRAM_DATA:
                         mCamera.sendHistogramData();
                         break;
+
+                    case SET_LONGSHOT:
+                        mCamera.setLongshot((Boolean) msg.obj);
+                        break;
+
                     default:
                         throw new RuntimeException("Invalid CameraProxy message=" + msg.what);
                 }
@@ -552,6 +559,13 @@ class AndroidCameraManagerImpl implements CameraManager {
             mCameraHandler.obtainMessage(
                     ENABLE_SHUTTER_SOUND, (enable ? 1 : 0), 0).sendToTarget();
         }
+
+        @Override
+        public void setLongshot(boolean enable) {
+            mCameraHandler.obtainMessage(SET_LONGSHOT,
+                    new Boolean(enable)).sendToTarget();
+        }
+
         @Override
         public void setHistogramMode(CameraDataCallback cb) {
             mCameraHandler.obtainMessage(SET_HISTOGRAM_MODE, cb).sendToTarget();

src/com/android/camera/CameraManager.java
@@ -361,5 +361,12 @@ public interface CameraManager {
          *
         */
         public void sendHistogramData();
+        /**
+         * Enables/Disables longshot mode.
+         *
+         * @param enable   {@code true} to enable longshot mode,
+         *                 {@code false} to disable it.
+        */
+        public void setLongshot(boolean enable);
     }
 }

src/com/android/camera/FocusOverlayManager.java
@@ -580,4 +580,8 @@ public class FocusOverlayManager {
     public void setZslEnable(boolean value) {
         mZslEnabled = value;
     }
+
+    public boolean isZslEnabled() {
+        return mZslEnabled;
+    }
 }

src/com/android/camera/PhotoController.java
@@ -30,6 +30,8 @@ public interface PhotoController extends OnShutterButtonListener {
     public static final int SNAPSHOT_IN_PROGRESS = 3;
     // Switching between cameras.
     public static final int SWITCHING_CAMERA = 4;
+    // Longshot mode
+    public static final int LONGSHOT = 5;
 
     // returns the actual set zoom value
     public int onZoomChanged(int requestedZoom);

src/com/android/camera/Storage.java
@@ -82,6 +82,12 @@ public class Storage {
     public static Uri addImage(ContentResolver resolver, String title,
             long date, Location location, int orientation, ExifInterface exif,
             byte[] jpeg, int width, int height, String pictureFormat) {
+        int jpegLength = 0;
+
+        if (jpeg != null) {
+            jpegLength = jpeg.length;
+        }
+
         // Save the image.
         String path = generateFilepath(title, pictureFormat);
         if (exif != null && (pictureFormat == null ||
@@ -91,7 +97,7 @@ public class Storage {
             } catch (Exception e) {
                 Log.e(TAG, "Failed to write data", e);
             }
-        } else {
+        } else if (jpeg != null) {
             if (!(pictureFormat.equalsIgnoreCase("jpeg") || pictureFormat == null)) {
                  File dir = new File(RAW_DIRECTORY);
                  dir.mkdirs();
@@ -99,7 +105,7 @@ public class Storage {
             writeFile(path, jpeg);
         }
         return addImage(resolver, title, date, location, orientation,
-                jpeg.length, path, width, height, pictureFormat);
+                jpegLength, path, width, height, pictureFormat);
     }
 
     // Add the image to media store.

commit b2447232185a9058e2616e62d5457d474ac7b259

Camera2: fix long press shutter behavior
Long press shutter should perform auto focus and take the photo
when the shutter is released. This behavior is broken in
f5b1181157b6d7f65376c0068768f8f3f94b5d69 when longshot/burst mode
is added by CAF.

Fix by only setting the long click handler for longshot/burst when
it is enabled.

Change-Id: I5149134641ec790c03857b4ea23c43a9ff0adfda

src/com/android/camera/PhotoModule.java
@@ -185,7 +185,6 @@ public class PhotoModule
     private ContentProviderClient mMediaProviderClient;
     private boolean mFaceDetectionStarted = false;
 
-    private static final String PERSIST_LONG_ENABLE = "persist.camera.longshot.enable";
     private static final String PERSIST_LONG_SAVE = "persist.camera.longshot.save";
 
     // Constant from android.hardware.Camera.Parameters
@@ -1591,15 +1590,10 @@ public class PhotoModule
     @Override
     public void onShutterButtonLongClick() {
         if ((null != mCameraDevice) && ((mCameraState == IDLE) || (mCameraState == FOCUSING))) {
-            boolean enable = false;
-            enable = SystemProperties.getBoolean(PERSIST_LONG_ENABLE, false);
-            if ( enable ) {
-                enable = SystemProperties.getBoolean(PERSIST_LONG_SAVE, false);
-                mLongshotSave = enable;
-                mCameraDevice.setLongshot(true);
-                setCameraState(PhotoController.LONGSHOT);
-                mFocusManager.doSnap();
-            }
+            mLongshotSave = SystemProperties.getBoolean(PERSIST_LONG_SAVE, false);
+            mCameraDevice.setLongshot(true);
+            setCameraState(PhotoController.LONGSHOT);
+            mFocusManager.doSnap();
         }
     }
 

src/com/android/camera/PhotoUI.java
@@ -43,6 +43,7 @@ import android.widget.FrameLayout.LayoutParams;
 import android.widget.ImageView;
 import android.widget.PopupWindow;
 import android.widget.Toast;
+import android.os.SystemProperties;
 
 import com.android.camera.CameraPreference.OnPreferenceChangedListener;
 import com.android.camera.FocusOverlayManager.FocusUI;
@@ -71,6 +72,7 @@ public class PhotoUI implements PieListener,
     CameraManager.CameraFaceDetectionCallback {
 
     private static final String TAG = "CAM_UI";
+    private static final String PERSIST_LONG_ENABLE = "persist.camera.longshot.enable";
     private static final int DOWN_SAMPLE_FACTOR = 4;
     private final AnimationManager mAnimationManager;
     private CameraActivity mActivity;
@@ -500,7 +502,8 @@ public class PhotoUI implements PieListener,
     public  void initializeFirstTime() {
         // Initialize shutter button.
         mShutterButton.setImageResource(R.drawable.btn_new_shutter);
-        mShutterButton.setOnShutterButtonListener(mController);
+        mShutterButton.setOnShutterButtonListener(mController,
+                SystemProperties.getBoolean(PERSIST_LONG_ENABLE, false));
         mShutterButton.setVisibility(View.VISIBLE);
     }
 

src/com/android/camera/ShutterButton.java
@@ -63,9 +63,11 @@ public class ShutterButton extends ImageView {
         super(context, attrs);
     }
 
-    public void setOnShutterButtonListener(OnShutterButtonListener listener) {
+    public void setOnShutterButtonListener(OnShutterButtonListener listener, boolean longClick) {
         mListener = listener;
-        setOnLongClickListener(mLongClick);
+        if (longClick) {
+            setOnLongClickListener(mLongClick);
+        }
     }
 
     @Override

src/com/android/camera/VideoUI.java
@@ -498,7 +498,7 @@ public class VideoUI implements PieRenderer.PieListener,
     private void initializeMiscControls() {
         mReviewImage = (ImageView) mRootView.findViewById(R.id.review_image);
         mShutterButton.setImageResource(R.drawable.btn_new_shutter_video);
-        mShutterButton.setOnShutterButtonListener(mController);
+        mShutterButton.setOnShutterButtonListener(mController, false);
         mShutterButton.setVisibility(View.VISIBLE);
         mShutterButton.requestFocus();
         mShutterButton.enableTouch(true);

src/com/android/camera/WideAnglePanoramaUI.java
@@ -377,7 +377,7 @@ public class WideAnglePanoramaUI implements
 
         mShutterButton = (ShutterButton) mRootView.findViewById(R.id.shutter_button);
         mShutterButton.setImageResource(R.drawable.btn_new_shutter);
-        mShutterButton.setOnShutterButtonListener(this);
+        mShutterButton.setOnShutterButtonListener(this, false);
         // Hide menu and indicators.
         mRootView.findViewById(R.id.menu).setVisibility(View.GONE);
         mRootView.findViewById(R.id.on_screen_indicators).setVisibility(View.GONE);