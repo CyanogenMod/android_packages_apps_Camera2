57fc34753f43f6320799fb40aea34ca90b120cfa
Vijay kumar Tumati <vtumati@codeaurora.org>
2014-01-10 12:24:16 +0530

    Camera: Change copy right year back to 2012
    As this is 3rd party file, we shouldn't
    change copy right year. Reverting it
    back to 2012.
    
    Change-Id: If617e7607cd89b0270aabbe1186190dfe06fe40c

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 The Android Open Source Project
+ * Copyright (C) 2012 The Android Open Source Project
  * Copyright (C) 2014 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
