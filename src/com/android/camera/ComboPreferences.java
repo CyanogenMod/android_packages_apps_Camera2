413224aee62bb53aa612cc1d9897a298079f934d
Michael Bestas <mikeioannina@gmail.com>
2014-01-20 20:40:32 +0200

    Camera: Powerkey shutter (2/2)
    Ported from cm-10.1
    
    Including cm-10.1 camera commit:
    aa4ae80a41fbab763891c5ef6d67a9e5b4bb981b Don't mess around with pref local ID for power shutter initialization.
    
    Includes http://review.cyanogenmod.org/56986
    PS2: Make Camera2 a privileged app
    PS5: Sign with platform certificate instead of making it a privileged app.
    PS6: Add write permission to PREVENT_POWER_KEY
    
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    Change-Id: I0992baa558eefd306d00fbece59cb5c512d9448b

--- src/com/android/camera/ComboPreferences.java
+++ src/com/android/camera/ComboPreferences.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2010 The Android Open Source Project
+ * Copyright (C) 2013-2014 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -153,6 +154,7 @@ public class ComboPreferences implements
                 || key.equals(CameraSettings.KEY_TIMER)
                 || key.equals(CameraSettings.KEY_TIMER_SOUND_EFFECTS)
                 || key.equals(CameraSettings.KEY_PHOTOSPHERE_PICTURESIZE)
+                || key.equals(CameraSettings.KEY_POWER_SHUTTER)
                 || key.equals(CameraSettings.KEY_STORAGE);
     }
 
