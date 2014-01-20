61ac387eb62e51db3030adfac9245d1e58d353a0
Michael Bestas <mikeioannina@gmail.com>
2014-01-20 20:40:32 +0200

    Camera: Powerkey shutter (2/2)
    Ported from cm-10.1
    
    Including cm-10.1 camera commit:
    aa4ae80a41fbab763891c5ef6d67a9e5b4bb981b Don't mess around with pref local ID for power shutter initialization.
    
    Includes http://review.cyanogenmod.org/56986
    PS2: Make Camera2 a privileged app
    PS5: Sign with platform certificate instead of making it a privileged app.
    
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    Change-Id: I0992baa558eefd306d00fbece59cb5c512d9448b

--- Android.mk
+++ Android.mk
@@ -22,6 +22,7 @@ LOCAL_AAPT_FLAGS := \
         --version-code $(version_code_package) \
 
 LOCAL_PACKAGE_NAME := Camera2
+LOCAL_CERTIFICATE := platform
 
 #LOCAL_SDK_VERSION := current
 
--- src/com/android/camera/VideoModule.java
+++ src/com/android/camera/VideoModule.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2012 The Android Open Source Project
+ * Copyright (C) 2013 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -434,6 +435,9 @@ public class VideoModule implements CameraModule,
         if (mActivity.setStoragePath(mPreferences)) {
             mActivity.updateStorageSpaceAndHint();
         }
+
+        // Power shutter
+        mActivity.initPowerShutter(mPreferences);
 
         /*
          * To reduce startup time, we start the preview in another thread.
@@ -1195,6 +1199,11 @@ public class VideoModule implements CameraModule,
                 return true;
             case KeyEvent.KEYCODE_CAMERA:
                 mUI.pressShutter(false);
+                return true;
+            case KeyEvent.KEYCODE_POWER:
+                if (CameraActivity.mPowerShutter) {
+                    onShutterButtonClick();
+                }
                 return true;
         }
         return false;
@@ -2135,6 +2144,7 @@ public class VideoModule implements CameraModule,
                 setCameraParameters();
             }
             mUI.updateOnScreenIndicators(mParameters, mPreferences);
+            mActivity.initPowerShutter(mPreferences);
         }
     }
 
