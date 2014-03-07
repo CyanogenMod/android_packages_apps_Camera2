59953a2bd28fa476bcb200623fe21da2f64f46ad
Teresa Ko <teresako@google.com>
2014-03-07 15:43:28 -0800

    Move startSmartCamera call back into onPreviewStarted().  (onPreviewInitialDataReceived() doesn't seem to be called anymore.)
    Change-Id: I25455dd8e1076150be37e419c73d5e5447c144d2

--- src/com/android/camera/CameraActivity.java
+++ src/com/android/camera/CameraActivity.java
@@ -842,7 +842,6 @@ public class CameraActivity extends Activity
                 new CameraManager.CameraPreviewDataCallback() {
                     @Override
                     public void onPreviewFrame(byte[] data, CameraManager.CameraProxy camera) {
-                        mCurrentModule.onPreviewInitialDataReceived();
                         mCameraAppUI.onNewPreviewFrame();
                     }
                 });
