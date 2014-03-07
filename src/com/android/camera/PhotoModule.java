CyanogenMod/android_packages_apps_Camera2

commit 942890667aa08cc3c1305d7ba64c1340df1219db

Camera2: add option to prevend autofocus cancel if preview stopped
There are camera HALs which cannot handle the autofocus cancel call if the preview
stopped in startPreview(). Add an option for this HAL's to do not cancel autofocus in this cases.

Change-Id: I0767cc7279405d17afc0b00120cb43e5bb309220

src/com/android/camera/PhotoModule.java
@@ -2103,7 +2103,10 @@ public class PhotoModule
             // If the focus mode is continuous autofocus, call cancelAutoFocus to
             // resume it because it may have been paused by autoFocus call.
             if (CameraUtil.FOCUS_MODE_CONTINUOUS_PICTURE.equals(mFocusManager.getFocusMode())) {
-                mCameraDevice.cancelAutoFocus();
+                if (CameraUtil.cancelAutoFocusOnPreviewStopped()
+                        || mCameraState != PREVIEW_STOPPED) {
+                    mCameraDevice.cancelAutoFocus();
+                }
             }
             mFocusManager.setAeAwbLock(false); // Unlock AE and AWB.
         }

res/values/config.xml
@@ -27,4 +27,7 @@
     <bool name="needsSamsungHDRFormat">false</bool>
     <!-- Ignore bogus preferred preview size -->
     <bool name="ignorePreferredPreviewSizeForVideo">false</bool>
+    <!-- Cancel autofocus if preview stopped. If camera HAL does not support
+        it set this value to false -->
+    <bool name="cancelAutoFocusOnPreviewStopped">true</bool>
 </resources>

src/com/android/camera/util/CameraUtil.java
@@ -126,6 +126,7 @@ public class CameraUtil {
     // Do not change the focus mode when TTF is used
     private static boolean sNoFocusModeChangeForTouch;
 
+    private static boolean sCancelAutoFocusOnPreviewStopped;
 
     // Fields for the show-on-maps-functionality
     private static final String MAPS_PACKAGE_NAME = "com.google.android.apps.maps";
@@ -136,6 +137,10 @@ public class CameraUtil {
 
     public static boolean isZSLEnabled() {
         return sEnableZSL;
+    }
+
+    public static boolean cancelAutoFocusOnPreviewStopped() {
+        return sCancelAutoFocusOnPreviewStopped;
     }
 
     public static boolean isSupported(String value, List<String> supported) {
@@ -220,6 +225,8 @@ public class CameraUtil {
         sEnableZSL = context.getResources().getBoolean(R.bool.enableZSL);
         sNoFocusModeChangeForTouch = context.getResources().getBoolean(
                 R.bool.useContinuosFocusForTouch);
+        sCancelAutoFocusOnPreviewStopped =
+                context.getResources().getBoolean(R.bool.cancelAutoFocusOnPreviewStopped);
         sSamsungHDRFormat = context.getResources().getBoolean(R.bool.needsSamsungHDRFormat);
         sDeviceKeysPresent = context.getResources().getInteger(
                 com.android.internal.R.integer.config_deviceHardwareKeys);

commit e51b074808807be847ac9207029fe72f6f68c23f

Camera2: Do not issue autofocus cancel if preview stopped
942890667aa08cc3c1305d7ba64c1340df1219db introduced the option to not
issue autofocus cancel if the preview was stopped. Extend it to the case
where the preview is stopped because the camera was called as an intent
(ie from MMS client)

Change-Id: I30032d8375665ecf6be471bf480c1f118d25423b

src/com/android/camera/PhotoModule.java
@@ -1018,8 +1018,9 @@ public class PhotoModule
             }else if ((mReceivedSnapNum == mBurstSnapNum)
                         && (mCameraState != LONGSHOT)) {
                 mFocusManager.restartTouchFocusTimer();
-                if (CameraUtil.FOCUS_MODE_CONTINUOUS_PICTURE.equals(
-                        mFocusManager.getFocusMode())) {
+                if ((CameraUtil.FOCUS_MODE_CONTINUOUS_PICTURE.equals(
+                        mFocusManager.getFocusMode())) && (CameraUtil.cancelAutoFocusOnPreviewStopped()
+                        || mCameraState != PREVIEW_STOPPED)) {
                     mCameraDevice.cancelAutoFocus();
                 }
                 mUI.resumeFaceDetection();