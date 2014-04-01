9eb517f4c4bcf00fbe435a56f89d3abcaaa4d881
Doris Liu <tianliu@google.com>
2014-04-01 15:53:56 -0700

    Temp swipe interception to support swipe from capture button
    Bug: 13535296
    Change-Id: I911336ce407abbfe51759bb38ffff9a534a49de6

--- src/com/android/camera/ui/MainActivityLayout.java
+++ src/com/android/camera/ui/MainActivityLayout.java
@@ -16,28 +16,67 @@
 
 package com.android.camera.ui;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewConfiguration;
 import android.widget.FrameLayout;
 
 import com.android.camera.debug.Log;
+import com.android.camera.util.UsageStatistics;
+import com.android.camera.widget.FilmstripLayout;
+import com.android.camera2.R;
+
+import com.google.common.logging.eventprotos;
 
 public class MainActivityLayout extends FrameLayout {
 
-    private MotionEvent mDown;
     private final Log.Tag TAG = new Log.Tag("MainActivityLayout");
+    // Only check for intercepting touch events within first 500ms
+    private static final int SWIPE_TIME_OUT = 500;
+
+    private ModeListView mModeList;
+    private FilmstripLayout mFilmstripLayout;
+    private boolean mCheckToIntercept;
+    private MotionEvent mDown;
+    private final int mSlop;
     private boolean mRequestToInterceptTouchEvents = false;
     private View mTouchReceiver = null;
+    private final boolean mIsCaptureIntent;
+
+    // TODO: This can be removed once we come up with a new design for b/13751653.
+    @Deprecated
+    private boolean mSwipeEnabled = true;
 
     public MainActivityLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
+        mSlop = ViewConfiguration.get(context).getScaledTouchSlop();
+
+        Activity activity = (Activity) context;
+        Intent intent = activity.getIntent();
+        String action = intent.getAction();
+        mIsCaptureIntent = (MediaStore.ACTION_IMAGE_CAPTURE.equals(action)
+                || MediaStore.ACTION_IMAGE_CAPTURE_SECURE.equals(action)
+                || MediaStore.ACTION_VIDEO_CAPTURE.equals(action));
+    }
+
+    /**
+     * Enables or disables the swipe for modules not supporting the new swipe
+     * logic yet.
+     */
+    @Deprecated
+    public void setSwipeEnabled(boolean enabled) {
+        mSwipeEnabled = enabled;
     }
 
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
         if (ev.getActionMasked() == MotionEvent.ACTION_DOWN) {
+            mCheckToIntercept = true;
             mDown = MotionEvent.obtain(ev);
             mTouchReceiver = null;
             mRequestToInterceptTouchEvents = false;
@@ -48,7 +87,38 @@ public class MainActivityLayout extends FrameLayout {
             return true;
         } else if (ev.getActionMasked() == MotionEvent.ACTION_POINTER_DOWN) {
             // Do not intercept touch once child is in zoom mode
+            mCheckToIntercept = false;
             return false;
+        } else {
+            // TODO: This can be removed once we come up with a new design for b/13751653.
+            if (!mCheckToIntercept) {
+                return false;
+            }
+            if (ev.getEventTime() - ev.getDownTime() > SWIPE_TIME_OUT) {
+                return false;
+            }
+            if (mIsCaptureIntent || !mSwipeEnabled) {
+                return false;
+            }
+            int deltaX = (int) (ev.getX() - mDown.getX());
+            int deltaY = (int) (ev.getY() - mDown.getY());
+            if (ev.getActionMasked() == MotionEvent.ACTION_MOVE
+                    && Math.abs(deltaX) > mSlop) {
+                // Intercept right swipe
+                if (deltaX >= Math.abs(deltaY) * 2) {
+                    mTouchReceiver = mModeList;
+                    onTouchEvent(mDown);
+                    return true;
+                }
+                // Intercept left swipe
+                else if (deltaX < -Math.abs(deltaY) * 2) {
+                    mTouchReceiver = mFilmstripLayout;
+                    UsageStatistics.changeScreen(eventprotos.NavigationChange.Mode.FILMSTRIP,
+                            eventprotos.CameraEvent.InteractionCause.SWIPE_LEFT);
+                    onTouchEvent(mDown);
+                    return true;
+                }
+            }
         }
         return false;
     }
@@ -60,6 +130,12 @@ public class MainActivityLayout extends FrameLayout {
             return mTouchReceiver.dispatchTouchEvent(ev);
         }
         return false;
+    }
+
+    @Override
+    public void onFinishInflate() {
+        mModeList = (ModeListView) findViewById(R.id.mode_list_layout);
+        mFilmstripLayout = (FilmstripLayout) findViewById(R.id.filmstrip_layout);
     }
 
     public void redirectTouchEventsTo(View touchReceiver) {
