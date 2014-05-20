3c33ca256349fabf9cea404291f60ae261860441
Alan Newberger <alann@google.com>
2014-05-20 14:34:53 -0700

    Port Glide loading to use Files rather than String URIs
    We encountered a bug with colons in filenames. Current integration
    passes file paths as basic URIs into Glide, which then gets
    confuses detecting paths with colons as they parse into schemes.
    This CL instead bakes the assumption that LocalMedia paths are
    file paths, into the app level. Tested with all modes and
    captures, formerly buggy filmstrip items now display.
    
    Bug: 15025417
    Change-Id: I91efa1e7b66d630e402b23a36b2a05c9783370a3

--- src/com/android/camera/data/ImageModelLoader.java
+++ src/com/android/camera/data/ImageModelLoader.java
@@ -7,25 +7,30 @@ import com.bumptech.glide.loader.bitmap.model.stream.StreamModelLoader;
 import com.bumptech.glide.loader.bitmap.resource.ResourceFetcher;
 
 import java.io.InputStream;
+import java.io.File;
 
 /**
  * Translates a local data representing an image into an InputStream that can be loaded by the
  * Glide library.
  */
 public class ImageModelLoader implements StreamModelLoader<LocalData> {
-    private final ModelLoader<String, InputStream> mPathLoader;
+    private final ModelLoader<File, InputStream> mPathLoader;
 
     public ImageModelLoader(Context context) {
-        mPathLoader = Glide.buildStreamModelLoader(String.class, context);
+        mPathLoader = Glide.buildStreamModelLoader(File.class, context);
     }
 
     @Override
     public ResourceFetcher<InputStream> getResourceFetcher(LocalData model, int width, int height) {
-        return mPathLoader.getResourceFetcher(model.getPath(), width, height);
+        return mPathLoader.getResourceFetcher(new File(nonNull(model.getPath())), width, height);
     }
 
     @Override
     public String getId(LocalData model) {
-        return mPathLoader.getId(model.getPath()) + model.getSignature();
+        return mPathLoader.getId(new File(nonNull(model.getPath()))) + model.getSignature();
+    }
+
+    private static String nonNull(String input) {
+        return (input != null) ? input : "";
     }
 }
--- src/com/android/camera/data/VideoModelLoader.java
+++ src/com/android/camera/data/VideoModelLoader.java
@@ -7,23 +7,29 @@ import com.bumptech.glide.loader.bitmap.model.ModelLoader;
 import com.bumptech.glide.loader.bitmap.model.file_descriptor.FileDescriptorModelLoader;
 import com.bumptech.glide.loader.bitmap.resource.ResourceFetcher;
 
+import java.io.File;
+
 /**
  * Translates a video data into an InputStream for the Glide library.
  */
 public class VideoModelLoader implements FileDescriptorModelLoader<LocalMediaData.VideoData> {
-    private final ModelLoader<String, ParcelFileDescriptor> mPathLoader;
+    private final ModelLoader<File, ParcelFileDescriptor> mPathLoader;
 
     public VideoModelLoader(Context context) {
-        mPathLoader = Glide.buildFileDescriptorModelLoader(String.class, context);
+        mPathLoader = Glide.buildFileDescriptorModelLoader(File.class, context);
     }
 
     @Override
     public ResourceFetcher<ParcelFileDescriptor> getResourceFetcher(LocalMediaData.VideoData model,
             int width, int height) {
-        return mPathLoader.getResourceFetcher(model.getPath(), width, height);
+        return mPathLoader.getResourceFetcher(new File(nonNull(model.getPath())), width, height);
     }
 
     public String getId(LocalMediaData.VideoData model) {
-        return mPathLoader.getId(model.getPath()) + model.getSignature();
+        return mPathLoader.getId(new File(nonNull(model.getPath()))) + model.getSignature();
+    }
+
+    private static String nonNull(String input) {
+        return (input != null) ? input : "";
     }
 }
