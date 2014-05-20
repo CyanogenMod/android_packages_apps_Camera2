4333fac867f6f6a11235b16d305dd197093b44d5
Sascha Haeberling <haeberling@google.com>
2014-05-20 16:28:11 -0700

    Show timer option in HDR+ mode and as disabled.
      Bug: 15089347
    
    Change-Id: I366cd0b553808d55136c141bc1efec6ada9ee9b0

--- src/com/android/camera/app/CameraAppUI.java
+++ src/com/android/camera/app/CameraAppUI.java
@@ -446,9 +446,16 @@ public class CameraAppUI implements ModeListView.ModeSwitchListener,
         public float exposureCompensationStep;
 
         /**
-         * Whether or not timer should show.
+         * Whether self-timer is enabled.
          */
         public boolean enableSelfTimer = false;
+
+        /**
+         * Whether the option for self-timer should show. If true and
+         * {@link #enableSelfTimer} is false, then the option should be shown
+         * disabled.
+         */
+        public boolean showSelfTimer = false;
     }
 
 
@@ -1756,7 +1763,11 @@ public class CameraAppUI implements ModeListView.ModeSwitchListener,
         if (bottomBarSpec.enableSelfTimer) {
             buttonManager.initializeButton(ButtonManager.BUTTON_COUNTDOWN, null);
         } else {
-            buttonManager.hideButton(ButtonManager.BUTTON_COUNTDOWN);
+            if (bottomBarSpec.showSelfTimer) {
+                buttonManager.disableButton(ButtonManager.BUTTON_COUNTDOWN);
+            } else {
+                buttonManager.hideButton(ButtonManager.BUTTON_COUNTDOWN);
+            }
         }
 
         if (bottomBarSpec.enablePanoOrientation
