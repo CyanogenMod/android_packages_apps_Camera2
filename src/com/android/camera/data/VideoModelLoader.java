837d8f2687cdf53e657c03c1d01b1c5df3902442
Sam Judd <judds@google.com>
2014-05-23 13:43:33 -0700

    Include signature in cache keys for Glide.
    Bug: 14625920
    Change-Id: Id010bae14ac163523215e3afad712a423aad5875

--- src/com/android/camera/data/VideoModelLoader.java
+++ src/com/android/camera/data/VideoModelLoader.java
@@ -1,10 +1,10 @@
 package com.android.camera.data;
 
 import android.content.Context;
+import android.net.Uri;
 import android.os.ParcelFileDescriptor;
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.loader.bitmap.model.ModelLoader;
 import com.bumptech.glide.loader.bitmap.model.file_descriptor.FileDescriptorModelLoader;
+import com.bumptech.glide.loader.bitmap.resource.FileDescriptorLocalUriFetcher;
 import com.bumptech.glide.loader.bitmap.resource.ResourceFetcher;
 
 import java.io.File;
@@ -13,23 +13,53 @@ import java.io.File;
  * Translates a video data into an InputStream for the Glide library.
  */
 public class VideoModelLoader implements FileDescriptorModelLoader<LocalMediaData.VideoData> {
-    private final ModelLoader<File, ParcelFileDescriptor> mPathLoader;
+    private final Context context;
 
     public VideoModelLoader(Context context) {
-        mPathLoader = Glide.buildFileDescriptorModelLoader(File.class, context);
+        this.context = context;
     }
 
     @Override
     public ResourceFetcher<ParcelFileDescriptor> getResourceFetcher(LocalMediaData.VideoData model,
             int width, int height) {
-        return mPathLoader.getResourceFetcher(new File(nonNull(model.getPath())), width, height);
+        return new UriFetcherWithId(context, getUri(model), model.getSignature());
     }
 
     public String getId(LocalMediaData.VideoData model) {
-        return mPathLoader.getId(new File(nonNull(model.getPath()))) + model.getSignature();
+        return model.getPath() + model.getSignature();
+    }
+
+    private static Uri getUri(LocalData model) {
+        return Uri.fromFile(new File(nonNull(model.getPath())));
     }
 
     private static String nonNull(String input) {
         return (input != null) ? input : "";
+    }
+
+    private static class UriFetcherWithId implements ResourceFetcher<ParcelFileDescriptor> {
+
+        private final FileDescriptorLocalUriFetcher uriFetcher;
+        private final String signature;
+
+        public UriFetcherWithId(Context context, Uri uri, String signature) {
+            uriFetcher = new FileDescriptorLocalUriFetcher(context, uri);
+            this.signature = signature;
+        }
+
+        @Override
+        public ParcelFileDescriptor loadResource() throws Exception {
+            return uriFetcher.loadResource();
+        }
+
+        @Override
+        public String getId() {
+            return uriFetcher.getId() + signature;
+        }
+
+        @Override
+        public void cancel() {
+            uriFetcher.cancel();
+        }
     }
 }
