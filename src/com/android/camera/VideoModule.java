85211380e9bc7aabaa5a6600a73f708459fe16f3
Suman Mukherjee <sumam@codeaurora.org>
2014-05-29 12:20:47 +0530

    Camera: Move out TimeLapse option from global list
    TimeLapse setting was common for front and back camera
    As there is no specific reason to have it common, to align with
    other settings, moved it out from common list so that Timelapse
    setting change in front camrea will not affect for back camera
    and vice versa.
    
    CRs-Fixed: 654627
    Change-Id: I0b14284adc6063ccb9bf175a37644c162bb47fd5

--- src/com/android/camera/VideoModule.java
+++ src/com/android/camera/VideoModule.java
@@ -2284,6 +2284,9 @@ public class VideoModule implements CameraModule,
         // onFrameAvailable from the old camera may already exist.
         mHandler.sendEmptyMessage(SWITCH_CAMERA_START_ANIMATION);
         mUI.updateOnScreenIndicators(mParameters, mPreferences);
+
+        //Display timelapse msg depending upon selection in front/back camera.
+        mUI.showTimeLapseUI(mCaptureTimeLapse);
     }
 
     private void initializeCapabilities() {
