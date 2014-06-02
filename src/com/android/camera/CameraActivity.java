ebba3288da5628c9ea7579b63139aa7b7312e137
Kevin Gabayan <gabayan@google.com>
2014-06-02 14:48:20 -0700

    MotionManager for motion-aware focus unlock in Camera2 Bug: 14222601
    Change-Id: Id118e12c8f789c74c8fc2e32ec208ff943b97e49

--- src/com/android/camera/CameraActivity.java
+++ src/com/android/camera/CameraActivity.java
@@ -43,13 +43,7 @@ import android.net.Uri;
 import android.nfc.NfcAdapter;
 import android.nfc.NfcAdapter.CreateBeamUrisCallback;
 import android.nfc.NfcEvent;
-import android.os.AsyncTask;
-import android.os.Build;
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.HandlerThread;
-import android.os.Looper;
-import android.os.Message;
+import android.os.*;
 import android.provider.MediaStore;
 import android.provider.Settings;
 import android.util.CameraPerformanceTracker;
@@ -78,6 +72,7 @@ import com.android.camera.app.LocationManager;
 import com.android.camera.app.MemoryManager;
 import com.android.camera.app.MemoryQuery;
 import com.android.camera.app.ModuleManagerImpl;
+import com.android.camera.app.MotionManager;
 import com.android.camera.app.OrientationManager;
 import com.android.camera.app.OrientationManagerImpl;
 import com.android.camera.cameradevice.CameraManager;
@@ -252,6 +247,7 @@ public class CameraActivity extends Activity
         }
     };
     private MemoryManager mMemoryManager;
+    private MotionManager mMotionManager;
 
     @Override
     public CameraAppUI getCameraAppUI() {
@@ -1383,6 +1379,7 @@ public class CameraActivity extends Activity
                         MemoryQuery.REPORT_LABEL_LAUNCH);
             }
         });
+        mMotionManager = getServices().getMotionManager();
     }
 
     /**
@@ -1526,6 +1523,8 @@ public class CameraActivity extends Activity
         mPreloader.cancelAllLoads();
         resetScreenOn();
 
+        mMotionManager.stop();
+
         UsageStatistics.instance().backgrounded();
 
         super.onPause();
@@ -1667,6 +1666,8 @@ public class CameraActivity extends Activity
 
         final int previewVisibility = getPreviewVisibility();
         updatePreviewRendering(previewVisibility);
+
+        mMotionManager.start();
     }
 
     private void fillTemporarySessions() {
