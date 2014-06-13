3bcb1cbdd4a7d67e276ef43dc6328dbdf397cbb8
sssemil <suleymanovemil8@gmail.com>
2014-06-13 16:45:33 +0500

    Camera2: Rotate only views
    Rotating everything pauses app
    
    Change-Id: Id1f726d1037651284535a07902affe8c6869a2ac

--- src/com/android/camera/PhotoMenu.java
+++ src/com/android/camera/PhotoMenu.java
@@ -18,6 +18,7 @@
 package com.android.camera;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.hardware.Camera.Parameters;
 import android.view.LayoutInflater;
