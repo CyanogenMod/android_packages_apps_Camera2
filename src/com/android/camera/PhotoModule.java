1bf0889d8a40ef9f1a56439563ae76688fd69716
Sascha Haeberling <haeberling@google.com>
2014-06-18 09:38:06 -0700

    Remote shutter should focus before taking a photo.
      Bug: 13929364
    
    Change-Id: Ib6fd83186100c67aef613f93bb8f9ea780da9ceb

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -58,7 +58,6 @@ import com.android.camera.app.MemoryManager;
 import com.android.camera.app.MemoryManager.MemoryListener;
 import com.android.camera.app.MotionManager;
 import com.android.camera.cameradevice.CameraCapabilities;
-import com.android.camera.cameradevice.CameraManager;
 import com.android.camera.cameradevice.CameraManager.CameraAFCallback;
 import com.android.camera.cameradevice.CameraManager.CameraAFMoveCallback;
 import com.android.camera.cameradevice.CameraManager.CameraPictureCallback;
@@ -1609,9 +1608,9 @@ public class PhotoModule
 
     @Override
     public void pause() {
-        getServices().getRemoteShutterListener().onModuleExit();
         mPaused = true;
         mHandler.removeCallbacks(mResumeTaskRunnable);
+        getServices().getRemoteShutterListener().onModuleExit();
         SessionStatsCollector.instance().sessionActive(false);
 
         Sensor gsensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
@@ -2304,7 +2303,7 @@ public class PhotoModule
 
     @Override
     public void onRemoteShutterPress() {
-        capture();
+        focusAndCapture();
     }
 
     /**
