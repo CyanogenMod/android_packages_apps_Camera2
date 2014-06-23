c87179c5c54ba95f6ea87e97245a1b9f20e0795d
Dheeraj CVR <cvr.dheeraj@gmail.com>
2014-06-23 22:00:07 +0530

    Camera2: Prevent propogating CancelAutoFocus during Video Recording
    When AutoFocus is started during Video Recording, CancelAutoFocus
    is called after KEY_FOCUS_TIME seconds. CancelAutoFocus has the
    side effect of resetting the sensor AF position and will result in
    instant loss of focus which is not a desired event when AF is
    manually triggered in case of Video Recording.
    
    Change-Id: Icee7f6dfd4ad083c9b383ee2a4df6a34e4668de6

--- src/com/android/camera/VideoModule.java
+++ src/com/android/camera/VideoModule.java
@@ -494,7 +494,6 @@ public class VideoModule implements CameraModule,
     @Override
     public void cancelAutoFocus() {
         if (null != mCameraDevice) {
-            mCameraDevice.cancelAutoFocus();
             setFocusParameters();
         }
     }
