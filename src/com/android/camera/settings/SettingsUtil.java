20bfecb92d4d4996fe722bedbc1912b5867f4931
Michael Bestas <mikeioannina@gmail.com>
2014-12-04 04:32:16 +0200

    Camera2: Add support for qcom camcorder resolutions
    Change-Id: Ie7e9a2a6256504a5af92a015fe3376c4d0cc49a9

--- src/com/android/camera/settings/SettingsUtil.java
+++ src/com/android/camera/settings/SettingsUtil.java
@@ -132,11 +132,16 @@ public class SettingsUtil {
 
     /** Video qualities sorted by size. */
     public static int[] sVideoQualities = new int[] {
+            CamcorderProfile.QUALITY_4kDCI,
             CamcorderProfile.QUALITY_2160P,
             CamcorderProfile.QUALITY_1080P,
             CamcorderProfile.QUALITY_720P,
+            CamcorderProfile.QUALITY_FWVGA,
+            CamcorderProfile.QUALITY_WVGA,
             CamcorderProfile.QUALITY_480P,
+            CamcorderProfile.QUALITY_VGA,
             CamcorderProfile.QUALITY_CIF,
+            CamcorderProfile.QUALITY_WQVGA,
             CamcorderProfile.QUALITY_QVGA,
             CamcorderProfile.QUALITY_QCIF
     };
